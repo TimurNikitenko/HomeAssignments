50c34f9c0cf94d5c9093ca13285323c644bced3e
Timur Nikitemko <timka.nikitenko@mail.ru>
2024-10-10 05:46:20 +0000

    Created new directory for first part of home assignment and created cpp file for consequent work

---
+++ Assignment2a/changing_file.cpp
@@ -0,0 +1,17 @@
+/* Nikitenko Timur st128375@student.spbu.ru
+ * first part of second home assignment
+ */
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+	std::ifstream infile;
+	infule.open("source.pdf", std::ios::binary|std::ios::in);
+	
+	
+	
+	
+	
+	return 0;
+}
