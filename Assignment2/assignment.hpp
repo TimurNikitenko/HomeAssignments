82a6e5ae37899ebfb4c07b898d059ef9ff8064d5
Timur Nikitemko <timka.nikitenko@mail.ru>
2024-10-10 09:28:12 +0000

    Created hpp file with functions declarations

---
+++ Assignment2/assignment.hpp
@@ -0,0 +1,13 @@
+/* Declaration of two functions
+ * for second home assignmnet
+ */
+
+#ifndef ASSIGNMENT.HPP
+#define ASSIGNMENT.HPP
+#include <iostream>
+
+int read_write(std::string file_name);
+
+int polish_calculate(std::string record);
+
+#endif // ASSIGNMENT.HPP
