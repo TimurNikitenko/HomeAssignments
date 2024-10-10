1c77326daa4a00ad1f75f9e9b3cb8606cbd57554
Timur Nikitemko <timka.nikitenko@mail.ru>
2024-10-10 07:29:12 +0000

    Updated main file, now it works in according to requirements

--- Assignment2a/changing_file.cpp
+++ Assignment2a/changing_file.cpp
@@ -4,14 +4,31 @@
 
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 int main() {
+	std::string file_name;
+	std::cout << "Enter file name: " << endl;
+	std::cin >> file_name;
+
 	std::ifstream infile;
-	infule.open("source.pdf", std::ios::binary|std::ios::in);
-	
-	
-	
-	
-	
+	infile.open(file_name, std::ios::binary|std::ios::in);
+
+	int size = std::filesystem::file_size(file_name);
+	int buffer[size];
+
+	infile.read((char *)&buffer, sizeof(buffer));
+	infile.close();
+
+	for (int i = 0; i < size; ++i) {
+		int temp = buffer[i];
+		buffer[i] = buffer[size-i];
+		buffer[size-i] = buffer[i];
+	}
+
+	std::ofstream outfile;
+	outfile.write((char *)&buffer, sizeof(buffer));
+	outfile.close();
+
 	return 0;
 }
